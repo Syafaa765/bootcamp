d052d2c47f3d3ce77023d4615d046d8f82ab188f
Syafaa765 <58631426+Syafaa765@users.noreply.github.com>
2020-06-22 19:46:23 +0700

    Add files via upload

---
+++ 003.js
@@ -0,0 +1,21 @@
+<script>
+Math.max(1,2,3,4,5,6);     // hasilnya: 6
+Math.max(10,20,30,90,5);   // hasilnya: 90
+Math.max(50);              // hasilnya: 50
+</script>
+
+<script>
+Math.min(1,2,3,4,5,6);       // hasilnya: 1
+Math.mmin(10,20,30,90,5);    // hasilnya: 5
+Math.max(50);                // hasilnya: 50
+</script>
+
+ <script>
+            function Hitung(dat1,dat2,dat3,dat4,dat5,dat6){
+                var total,totalrat;
+                total = dat1+dat2+dat3+dat4+dat5+dat6;
+              
+                totalrat = total / 6;
+                return totalrat;
+            }
+        </script>
