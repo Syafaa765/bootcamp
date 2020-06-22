509a380b2753d334f143d8091ce07b48270110bc
Syafaa765 <58631426+Syafaa765@users.noreply.github.com>
2020-06-22 14:56:20 +0700

    Update 004.js

--- 004.js
+++ 004.js
@@ -1,16 +1,12 @@
-function reverse(param){
-	var x = param.length, y = '';
-	while(x > 0){
-		y += param[x - 1];
-		x--;
-	}
-	return y;
-}
-var word = "Hello World";
-
-if(word === reverse(word)){
-	console.log("Kata `"+word+"` termasuk kata Palindrome.");
-}
-else{
-	console.log("Kata `"+word+"` termasuk bukan kata Palindrome.");
-}
+function palindrome(kata) {
+    let len = kata.length;
+    let leftSide = kata.slice(0, Math.floor(len/2));
+    let rightSide = kata.slice(Math.ceil(len/2), len).split('').reverse().join('');
+  
+    if (leftSide == rightSide) return true;
+    return false;
+  }
+  
+  // TEST CASES
+  console.log(palindrome('kasur rusak')); // true
+  console.log(palindrome('mister')); // false
