45737ae759685a18be3939fec41fcb19b82414a0
Syafaa765 <58631426+Syafaa765@users.noreply.github.com>
2020-06-22 12:13:55 +0700

    Create 002.js

---
+++ 002.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var bil=prompt("Masukkan bilangan : ")
+hasil=bil % 2
+if (hasil==0) {
+document.write("Genap!")
+}
+else {
+document.write("Ganjil!")
+}
+</script>
